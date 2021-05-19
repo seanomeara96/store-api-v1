d16f06e27163da9216ca92e8277d083b22a130e9
seanomeara96 <seanom96@gmail.com>
2021-05-19 14:11:48 +0100

    added config file

--- config/config.js
+++ config/config.js
@@ -1,18 +1,34 @@
 const dotenv = require("dotenv");
 const path = require("path");
+const axios = require("axios");
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
-const config = {
-  fs_xAuthTokenHeader: returnTokenHeader(process.env.FS_XAUTHTOKEN),
-  bf_xAuthTokenHeader: returnTokenHeader(process.env.BF_XAUTHTOKEN),
-  huk_xAuthTokenHeader: returnTokenHeader(process.env.HUK_XAUTHTOKEN),
-  bf: process.env.BF_STORE_HASH,
-  bsk: process.env.BSK_STORE_HASH,
-  bs: process.env.BS_STORE_HASH,
-  ah: process.env.AH_STORE_HASH,
-  fs: process.env.FS_STORE_HASH,
-  huk: process.env.HUK_STORE_HASH
+let api = {
+  store: undefined,
+  config: (storeInitials, version = 3) => {
+    storeInitials = storeInitials.toUpperCase();
+    let stores = [
+      "BF",
+      "BSK",
+      "AH",
+      "AS",
+      "HUK",
+      "HIE",
+      "IH",
+      "BS",
+      "PB",
+      "FS",
+    ];
+
+    if (!stores.includes(storeInitials))
+      throw new Error("Provide a valid store");
+
+    api.store = axios.create({
+      baseURL: `https://api.bigcommerce.com/stores/${
+        process.env[`${storeInitials}_STORE_HASH`]
+      }/v${version}`,
+      headers: { "x-auth-token": process.env[`${storeInitials}_XAUTHTOKEN`] },
+    });
+  },
 };
-function returnTokenHeader(authToken){
-  return { "x-auth-token": authToken }
-}
-module.exports = config;
+
+module.exports = api;
