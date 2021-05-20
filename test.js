a9a9e5105bb82eda86a6ec54fee37066b9406680
seanomeara96 <seanom96@gmail.com>
2021-05-20 16:01:22 +0100

    new test

--- test.js
+++ test.js
@@ -1,43 +1,6 @@
-const axios = require("axios");
-const config = require("./config/config");
-const instance = axios.create({
-  baseURL: `https://api.bigcommerce.com/stores/${config.bf}/v2`,
-  headers: config.bf_xAuthTokenHeader,
-});
-
-const getAll =
-  (URL) =>
-  (params = {}) =>
-    new Promise((resolve, reject) => {
-      let pageNumber = 1;
-      let aggregatedData = [];
-      async function getData() {
-        try {
-          const { data } = await instance.get(URL, {
-            params: {
-              limit: 250,
-              page: pageNumber,
-              ...params,
-            },
-          });
-          let dataArray = data;
-          if (dataArray.length) {
-            aggregatedData.push(...dataArray);
-            pageNumber++;
-            getData();
-          } else {
-            resolve(aggregatedData);
-          }
-        } catch (err) {
-          reject(err);
-        }
-      }
-      getData();
-    });
-
-const getAllBlogPosts = getAll("/blog/posts");
-
-getAllBlogPosts().then((blogPosts) => {
-  const results = blogPosts.map(({ body }) => body.includes("http://")).filter(bool => bool == true)
-  console.log(results.length);
-});
+const api = require("./config/config");
+api.config("bf");
+api.store
+  .get("/storefront/redirects")
+  .then((res) => console.log(res.data.length))
+  .catch((err) => console.log(err));
