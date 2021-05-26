cb67506b2c62eace9a64e365d9136194ea1613e8
seanomeara96 <seanom96@gmail.com>
2021-05-26 10:17:44 +0100

    restored working report

--- reports/gwpStockReport.js
+++ reports/gwpStockReport.js
@@ -1,45 +1,29 @@
-const api = require("../config/config")
+require("../config/config").config("bf")
 const { getManyProductsBySKU } = require("../products/getManyProductsBySKU");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-let skuArrays = {
-  bf: [
-    { sku: "GWP32" }, // pollution protection
-    { sku: "9448" }, // cashmere cream
-    { sku: "10122" }, // reshaping spray
-    { sku: "10151" }, // detangling comb
-    { sku: "9357" }, // caudalie hand nail cream
-    { sku: "9414" }, // one united 30ml
-    { sku: "8702" }, // clay cleanser
-    { sku: "9013" }, // tummy rub butter
-  ],
-  ih: [{ sku: "10195" }], // basq 30ml cooling lotion
-};
-let stores = Object.keys(skuArrays)
-stores.forEach(store => {
-  console.log(store)
-  api.config(store)
-  console.log((skuArrays[store]))
-  getManyProductsBySKU(skuArrays[store])
-  .then((res) => {
-   console.log(res)
-  })
-  .catch((err) => console.log(err));
-})
+let skuArray = [
+  { sku: "GWP32" }, // pollution protection
+  { sku: "9448" }, // cashmere cream
+  { sku: "10122" }, // reshaping spray
+  { sku: "10151" }, // detangling comb
+  { sku: "9357" }, // caudalie hand nail cream
+  { sku: "9414" }, // one united 30ml
+  { sku: "8702" }, // clay cleanser
+  { sku: "9013" }, // tummy rub butter
+];
 
-/**
- * 
- * 
- * 
- * 
- * const data = res
+getManyProductsBySKU(skuArray)
+  .then((res) => {
+    console.log(res)
+    const data = res
       .map(
         ({ name, sku, inventory_level }) =>
-          `<p>${name} (${sku}) => Inventory Level: <strong>${inventory_level}</strong></p>`
+          `<p>Name: ${name}, SKU: ${sku}, Inventory Level: ${inventory_level}</p>`
       )
       .join("\n");
     const msg = {
-      to: ["sean@beautyfeatures.ie", "brendan@beautyfeatures.ie"],
+      to: "sean@beautyfeatures.ie",
       from: "sean@beautyfeatures.ie",
       subject: "GWP Stock Report",
       text: "GWP Stock Report",
@@ -53,4 +37,5 @@ stores.forEach(store => {
       .catch((error) => {
         console.error(error);
       });
- */
+  })
+  .catch((err) => console.log(err));
