78317dfae17f992aad2022b4dda61879ea9ff896
seanomeara96 <seanom96@gmail.com>
2021-06-16 16:07:45 +0100

    can export categories

--- categories/getAllCategories.js
+++ categories/getAllCategories.js
@@ -1,6 +1,9 @@
+const {getAll} = require("../utils/getAll");
 /**
  * This function fetches all categories resolves with an array of objects
  * @param {*} params
  * @returns
  */
 exports.getAllCategories = getAll("/catalog/categories");
+
+//require("../config/config").config("bf");getAllCategories().then(res => console.log(res))
---
+++ exports/exportCats.js
@@ -0,0 +1,24 @@
+const { getAllCategories } = require("../categories/getAllCategories");
+const output = require("./utils/output");
+require("../config/config").config("bs");
+
+getAllCategories().then((res) => {
+  const cats = res.map((cat) => {
+    return {
+      ID: cat.id,
+      "Parent Id": cat.parent_id,
+      Name: cat.name,
+      "Has Page Title": cat.page_title ? "TRUE" : "FALSE",
+      "Has Meta Description": cat.meta_description ? "TRUE" : "FALSE",
+      "Has Content": cat.description ? "TRUE" : "FALSE",
+      "Page Title": cat.page_title,
+      "Page Title Length": cat.page_title.length,
+      "Meta Description": cat.meta_description,
+      "Meta Description Length": cat.meta_description.length,
+      URL: cat.custom_url.url,
+      "Meta Keywords": cat.meta_keywords.join(" "),
+      "Search Keywords": cat.search_keywords,
+    };
+  });
+  output("category", cats);
+});
