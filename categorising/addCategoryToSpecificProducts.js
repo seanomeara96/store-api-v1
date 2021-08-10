4f40401232495fae79a5be90809aa8c4fe813f08
seanomeara96 <seanom96@gmail.com>
2021-08-10 16:03:19 +0100

    added products to category

--- categorising/addCategoryToSpecificProducts.js
+++ categorising/addCategoryToSpecificProducts.js
@@ -1,41 +1,37 @@
-require("../config/config").config("hie");
+require("../config/config").config("huk");
 const { getProductById } = require("../products/getProductById");
 const { addCatToProduct } = require("../products/addCatToProduct");
 /**
  * issue with this script is that its prematurely returning an empty array and not the expected output from promise allsettled
  */
-const productIds = [{"Product ID":216},
-{"Product ID":192},
-{"Product ID":193},
-{"Product ID":217},
-{"Product ID":218},
-{"Product ID":219},
-{"Product ID":220},
-{"Product ID":221},
-{"Product ID":194},
-{"Product ID":195},
-{"Product ID":196},
-{"Product ID":197},
-{"Product ID":214},
-{"Product ID":215},
-{"Product ID":216},
-{"Product ID":217},
-{"Product ID":222},
-{"Product ID":198},
-{"Product ID":199},
-{"Product ID":200},
-{"Product ID":223},
-{"Product ID":201},
-{"Product ID":202},
-{"Product ID":203},
-{"Product ID":204},
-{"Product ID":218},
-{"Product ID":219},
-{"Product ID":220},
-{"Product ID":221},
-{"Product ID":224},
-{"Product ID":225}];
-const catId = 98;
+const productIds = [{"Product ID":385},
+{"Product ID":386},
+{"Product ID":387},
+{"Product ID":388},
+{"Product ID":389},
+{"Product ID":390},
+{"Product ID":391},
+{"Product ID":392},
+{"Product ID":393},
+{"Product ID":394},
+{"Product ID":396},
+{"Product ID":398},
+{"Product ID":399},
+{"Product ID":400},
+{"Product ID":401},
+{"Product ID":402},
+{"Product ID":403},
+{"Product ID":404},
+{"Product ID":405},
+{"Product ID":406},
+{"Product ID":407},
+{"Product ID":408},
+{"Product ID":409},
+{"Product ID":410},
+{"Product ID":411},
+{"Product ID":412},
+{"Product ID":413}]
+const catId = 68;
 /**
  * This needs to be tested before using
  * @param {*} productIds
@@ -70,5 +66,5 @@ const addCategoryToSpecificProducts = (productIds, categoryId) =>
   });
 
 addCategoryToSpecificProducts(productIds, catId)
-  .then((res) => console.log("addCategoryToSpecificProducts response",res))
+  .then((res) => console.log("addCategoryToSpecificProducts response", res))
   .catch((err) => console.log(err));
