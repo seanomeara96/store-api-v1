5c3a628372ea444463362e6480dee6115684b02e
seanomeara96 <seanom96@gmail.com>
2021-08-24 15:26:57 +0100

    will update all stores in sequence

--- barcodes/updateProductBarcodes.js
+++ barcodes/updateProductBarcodes.js
@@ -1,35 +1,43 @@
-require("../config/config").config("bf");
-const { getAllProducts } = require("../products/getAllProducts");
-const { updateProductBarcode } = require("./updateProductBarcode");
-const csv = require("csvtojson");
 /**
- *
+ * updates product barcodes, cant do options yet but products yes
  */
-async function updateProductBarcodes() {
-  try {
-    const products = await getAllProducts();
-    const upToDateBarcodes = await csv().fromFile("./bp.csv");
-    let promises = [];
-    products.forEach((bcProduct) => {
-      let bpProductDetails = upToDateBarcodes.find(
-        (bpDoc) => bpDoc.SKU === bcProduct.sku
-      );
-      if (!bpProductDetails) {
-        console.log(`${bcProduct.name} is either an old SKU or a config`);
-        return;
-      }
-      if (bcProduct.upc !== bpProductDetails.Barcode) {
-        console.log(`${bcProduct.name} barcode mismatch`.red);
-        promises.push(
-          updateProductBarcode(bcProduct.id, bpProductDetails.Barcode)
+function updateProductBarcodes(store) {
+  return new Promise(async (resolve, reject) => {
+    require("../config/config").config(store);
+    const { getAllProducts } = require("../products/getAllProducts");
+    const { updateProductBarcode } = require("./updateProductBarcode");
+    const csv = require("csvtojson");
+    const colors = require("colors");
+    try {
+      const products = await getAllProducts();
+      const upToDateBarcodes = await csv().fromFile("./bp.csv");
+      let promises = [];
+      products.forEach((bcProduct) => {
+        let bpProductDetails = upToDateBarcodes.find(
+          (bpDoc) => bpDoc.SKU === bcProduct.sku
         );
-      }
-    });
-    const responses = await Promise.allSettled(promises);
-    console.log(responses.length);
-  } catch (err) {
-    console.log(err);
-  }
+        if (!bpProductDetails) {
+          console.log(`${bcProduct.name} is either an old SKU or a config`);
+          return;
+        }
+        if (bcProduct.upc !== bpProductDetails.Barcode) {
+          console.log(`${bcProduct.name} barcode mismatch`.red);
+          promises.push(
+            updateProductBarcode(bcProduct.id, bpProductDetails.Barcode)
+          );
+        }
+      });
+      const responses = await Promise.allSettled(promises);
+      console.log(responses.length);
+      resolve()
+    } catch (err) {
+      console.log(err);
+      reject()
+    }
+  });
 }
+let stores = ["bf", "ah", "bsk", "pb", "bs", "huk"];
 
-updateProductBarcodes();
+stores = stores.map(store => ()=>updateProductBarcodes(store))
+
+stores.reduce((acc, cur) => acc.then(cur), Promise.resolve());
