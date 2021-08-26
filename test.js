56551a8b49264d1df8932a546e7a9bd2ad341ed4
seanomeara96 <seanom96@gmail.com>
2021-08-26 15:52:15 +0100

    new test

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-require("./config/config").config("bf")
-const { getProductById } = require("./products/getProductById");
-// getProductById(5298).then(console.log).catch(console.log)
-let product_id =5283 
-getProductById(product_id).then((product)=>{
-  console.log(product.name)
-  console.log(product.option_set_id)
-  if(product.option_set_id){
-    require("./config/config").store.get(`/catalog/products/${product_id}/variants`).then((data) => console.log(data.data.data.length))
-  }
-}).catch(console.log)
+require("./config/config").config("ih")
+const { getAllCategories} = require("./categories/getAllCategories")
+async function main (){
+  let cats = await getAllCategories();
+  
+  let catsWithImageCount = cats.filter(({image_url}) => image_url !== "")
+  console.log(cats.length)
+  console.log(catsWithImageCount)
+}
+main()
