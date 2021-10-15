03fb0fc6cf0d6bc3597f2829f69e5b1df4d43766
seanomeara96 <seanom96@gmail.com>
2021-10-15 08:49:42 +0100

    removed promotion

--- scripts/promotions/bf/moroccanoil/removePromotion.js
+++ scripts/promotions/bf/moroccanoil/removePromotion.js
@@ -1,16 +1,11 @@
-require("../../../config/config").config("bf");
+require("../../../../config/config").config("bf");
 const {
-  removeLineFromBrandProducts,
-} = require("../../../content/removeLineFromBrandProducts");
-const { bfGwpTemplate } = require("../../../templates/bfGwpTemplate");
+  removePromotionFromBrandProducts,
+} = require("../../../../functions/content/removePromotionFromBrandProducts");
+
 const brandName = "Moroccanoil";
-const lineToRemove = bfGwpTemplate(
-  brandName,
-  "https://www.beautyfeatures.ie/free-moroccanoil-gift-with-purchase/",
-  "Travel Essentials Kit",
-  80
-);
+
 //console.log(lineToRemove)
-removeLineFromBrandProducts(brandName, lineToRemove)
+removePromotionFromBrandProducts(brandName)
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
