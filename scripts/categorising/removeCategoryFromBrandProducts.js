d4cbc80ca97a244ce7be089f2a4f642a2b020447
seanomeara96 <seanom96@gmail.com>
2021-10-19 08:50:22 +0100

    can remove categor from products by brand

---
+++ scripts/categorising/removeCategoryFromBrandProducts.js
@@ -0,0 +1,31 @@
+require("../../config/config").config("ah");
+const { removeCatFromProduct } = require("../../functions/products/removeCatFromProduct");
+const {
+  getProductsByBrand,
+} = require("../../functions/products/getProductsByBrand");
+
+
+/**
+ * removes category from products by brand
+ * @param {*} categoryName
+ * @returns
+ */
+const removeCategoryFromBrandProducts = (brand, categoryId) =>
+  new Promise(async (resolve, reject) => {
+    let products;
+    try {
+      products = await getProductsByBrand(brand);
+    } catch (e) {
+      reject(e);
+    }
+    let promises = [];
+    products.forEach(({id}) => {
+      promises.push(removeCatFromProduct(id, categoryId));
+    });
+    Promise.allSettled(promises).then(resolve).catch(reject);
+  });
+const brand = "Olaplex";
+const catId = 173;
+removeCategoryFromBrandProducts(brand, catId)
+  .then((res) => console.log("removeCategoryFromAllProducts response", res))
+  .catch((err) => console.log(err));
--- scripts/reports/couponReport.js
+++ scripts/reports/couponReport.js
@@ -1,7 +1,7 @@
-require("../../config/config").config("bf", 2);
+require("../../config/config").config("ih", 2);
 const { getAllOrders } = require("../../functions/orders/getAllOrders");
 const { getOrderCoupon } = require("../../functions/orders/getOrderCoupon");
-const { ouput } = require("../utils/output");
+const { output } = require("../utils/output");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 let monthsToTest = [
@@ -17,77 +17,79 @@ let monthsToTest = [
   { monthName: "October", month: 9, lastDay: 31, year: 2020 },
   { monthName: "November", month: 10, lastDay: 30, year: 2020 },
   { monthName: "December", month: 11, lastDay: 31, year: 2020 },
-];
-let period = monthsToTest[7];
-// month base 0
-const startDate = new Date(period.year, period.month, 1);
-const endDate = new Date(period.year, period.month, period.lastDay);
-getAllOrders({
-  min_date_created: startDate.toISOString(),
-  max_date_created: endDate.toISOString(),
-})
-  .then(async (orders) => {
-    try {
-      let promises = [];
-      orders = orders.filter(
-        ({ coupon_discount }) => parseFloat(coupon_discount) > 0
-      );
-      console.log("orders with discount", orders.length);
-      orders.forEach((order) => {
-        promises.push(getOrderCoupon(order.id, order.coupons.resource));
-      });
-      console.log("number of promises", promises.length);
-      let res = await Promise.allSettled(promises);
-      let rejected = res.filter(({status}) => status === "rejected");
-      rejected.forEach(({reason}) => console.log(reason))
-      console.log(`${rejected.length} promises rejected / ${res.length} total responses`)
-      res = res.filter(i => i.status === "fulfilled")
-      res = res.map(({ value }) => value);
-      console.log("number of responses", res.length);
-      res.forEach(({ orderId, code }) => {
-        orders.find((order) => order.id === orderId)["coupon_used"] = code;
-      });
-      let allCouponsUsed = [
-        ...new Set(orders.map(({ coupon_used }) => coupon_used)),
-      ];
-      let totalRevenuePerCoupon = allCouponsUsed.map((coupon) => {
-        return {
-          coupon,
-          revenue: orders
-            .filter(({ coupon_used }) => coupon_used === coupon)
-            .reduce((a, b) => {
-              return a + parseFloat(b.subtotal_inc_tax);
-            }, 0),
-        };
-      });
-      const couponRank = totalRevenuePerCoupon.sort(
-        (a, b) => b.revenue - a.revenue
-      );
-      const output = await ouput(`${period.monthName}-couponUsage`, couponRank);
-      const attachment = Buffer.from(output).toString("base64");
-      const msg = {
-        to: "sean@beautyfeatures.ie",
-        from: "sean@beautyfeatures.ie",
-        subject: `Coupon Usage Stats for ${startDate.toDateString()} to ${endDate.toDateString()}`,
-        text: `Coupon Usage Stats for ${startDate.toDateString()} to ${endDate.toDateString()}`,
-        html: couponRank
-          .map(
-            ({ coupon, revenue }) =>
-              `<p><strong>Coupon: </strong><span>${coupon}</span>\t<strong>Revenue: </strong><span>${revenue}</span></p>`
-          )
-          .join("\n"),
-        attachments: [
-          {
-            content: attachment,
-            filename: "couponUsage-output.csv",
-            type: "text/csv",
-            disposition: "attachment",
-          },
-        ],
-      };
-      sgMail.send(msg).catch((err) => console.log(err.response.body));
-    } catch (err) {
-      console.log(err);
-    }
+]; // month base 0
+monthsToTest.forEach((period) => {
+  const startDate = new Date(period.year, period.month, 1);
+  const endDate = new Date(period.year, period.month, period.lastDay);
+  getAllOrders({
+    min_date_created: startDate.toISOString(),
+    max_date_created: endDate.toISOString(),
   })
-  .catch(console.log);
+    .then(async (orders) => {
+      try {
+        let promises = [];
+        orders = orders.filter(
+          ({ coupon_discount }) => parseFloat(coupon_discount) > 0
+        );
+        console.log("orders with discount", orders.length);
+        orders.forEach((order) => {
+          promises.push(getOrderCoupon(order.id, order.coupons.resource));
+        });
+        console.log("number of promises", promises.length);
+        let res = await Promise.allSettled(promises);
+        let rejected = res.filter(({ status }) => status === "rejected");
+        rejected.forEach(({ reason }) => console.log(reason));
+        console.log(
+          `${rejected.length} promises rejected / ${res.length} total responses`
+        );
+        res = res.filter((i) => i.status === "fulfilled");
+        res = res.map(({ value }) => value);
+        console.log("number of responses", res.length);
+        res.forEach(({ orderId, code }) => {
+          orders.find((order) => order.id === orderId)["coupon_used"] = code;
+        });
+        let allCouponsUsed = [
+          ...new Set(orders.map(({ coupon_used }) => coupon_used)),
+        ];
+        let totalRevenuePerCoupon = allCouponsUsed.map((coupon) => {
+          return {
+            coupon,
+            revenue: orders
+              .filter(({ coupon_used }) => coupon_used === coupon)
+              .reduce((a, b) => {
+                return a + parseFloat(b.subtotal_inc_tax);
+              }, 0),
+          };
+        });
+        const couponRank = totalRevenuePerCoupon.sort(
+          (a, b) => b.revenue - a.revenue
+        );
+        const out = await output(`${period.monthName}-couponUsage`, couponRank);
+        const attachment = Buffer.from(out).toString("base64");
+        const msg = {
+          to: "sean@beautyfeatures.ie",
+          from: "sean@beautyfeatures.ie",
+          subject: `Coupon Usage Stats for ${startDate.toDateString()} to ${endDate.toDateString()}`,
+          text: `Coupon Usage Stats for ${startDate.toDateString()} to ${endDate.toDateString()}`,
+          html: couponRank
+            .map(
+              ({ coupon, revenue }) =>
+                `<p><strong>Coupon: </strong><span>${coupon}</span>\t<strong>Revenue: </strong><span>${revenue}</span></p>`
+            )
+            .join("\n"),
+          attachments: [
+            {
+              content: attachment,
+              filename: "couponUsage-output.csv",
+              type: "text/csv",
+              disposition: "attachment",
+            },
+          ],
+        };
+        sgMail.send(msg).catch((err) => console.log(err.response.body));
+      } catch (err) {
+        console.log(err);
+      }
+    })
+    .catch(console.log);
+});
