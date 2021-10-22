3dfdf6196c147369a71300b928e914512305c14c
seanomeara96 <seanom96@gmail.com>
2021-10-22 10:19:14 +0100

    removed weird looking ternary operator

--- scripts/reports/dummyAudit.js
+++ scripts/reports/dummyAudit.js
@@ -1,6 +1,8 @@
-const { getAllCategories } = require("../../functions/categories/getAllCategories");
+const {
+  getAllCategories,
+} = require("../../functions/categories/getAllCategories");
 const { getAllProducts } = require("../../functions/products/getAllProducts");
-require("../../config/config")
+require("../../config/config");
 const { log, error } = console;
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -13,16 +15,19 @@ function checkInStockDummy(storeInit) {
       const dummyCategoryId = categories.find(
         ({ name }) => name.toLowerCase() === "dummy"
       ).id;
-      const liveProductsInDummy = products.filter(({ categories, is_visible }) =>
-        categories.includes(dummyCategoryId) && is_visible
+      const liveProductsInDummy = products.filter(
+        ({ categories, is_visible }) =>
+          categories.includes(dummyCategoryId) && is_visible
       );
       const productsInDummyAndInStockArray = liveProductsInDummy.filter(
         ({ inventory_level }) => inventory_level > 0
-      )
+      );
       const productsInDummyAndInStockString = productsInDummyAndInStockArray
         .map(({ name, sku }) => `<div><p>Name: ${name} (${sku})</p></div>`)
         .join("\n");
-      const finalHtml = productsInDummyAndInStockString.length ? productsInDummyAndInStockString : `<div><p>All Clear</p></div>`; 
+      const finalHtml = productsInDummyAndInStockString.length
+        ? productsInDummyAndInStockString
+        : `<div><p>All Clear</p></div>`;
       let storeInitials = `<div><strong>Store: ${storeInit.toUpperCase()}</strong></div>`;
       resolve(storeInitials + "\n" + finalHtml);
     } catch (err) {
@@ -43,7 +48,7 @@ function sendInStockDummyAllStoresEmail(responses) {
   sgMail
     .send(msg)
     .then(() => log("Email sent"))
-    .catch(err => error(err.response.body.errors));
+    .catch((err) => error(err.response.body.errors));
 }
 function checkInStockDummyAllStores() {
   const allStores = ["bf", "bsk", "ah", "pb", "ih", "bs", "huk", "hie"];
@@ -56,11 +61,10 @@ function checkInStockDummyAllStores() {
       acc
         .then(cur)
         .then((res) => responses.push(res))
-        .then(() =>
-          indx === allStores.length - 1
-            ? sendInStockDummyAllStoresEmail(responses)
-            : null
-        ),
+        .then(() => {
+          if (indx === allStores.length - 1)
+            sendInStockDummyAllStoresEmail(responses);
+        }),
     Promise.resolve()
   );
 }
