ef5d3284bca5b51dbbb5149f36207328e3c35f99
seanomeara96 <seanom96@gmail.com>
2021-10-28 15:58:39 +0100

    added comments

--- scripts/automations/removeRetailPrices.js
+++ scripts/automations/removeRetailPrices.js
@@ -5,11 +5,32 @@ const { log } = console;
 const productPrices = ({ id, price, retail_price, sale_price }) => {
   return { id, price, retail_price, sale_price };
 };
+/**
+ * returns any non zero retail prices
+ * @param {nuber} param0
+ * @returns
+ */
 const nonZeroRetailPrice = ({ retail_price }) => retail_price;
 
-const setRetailPriceToZero = ({ id }) =>
-  updateProduct(id, { retail_price: "0" });
+/**
+ * returns a function that sets a retial price to 0
+ * @param {number} param0 product id
+ * @returns
+ */
+const setRetailPriceToZero = ({ id }) => updateProduct(id, { retail_price: 0 });
 
+/**
+ * throws an err
+ * @param {err} err
+ */
+const throwErr = (err) => {
+  throw new Error(err);
+};
+
+/**
+ * resolves with an array of all products with a non-zero retail price
+ * @returns 
+ */
 function fetchNonZeroRetailPrices() {
   return new Promise(async (resolve, reject) => {
     try {
@@ -25,12 +46,16 @@ function fetchNonZeroRetailPrices() {
   });
 }
 
+/**
+ * removes retail prices
+ */
 async function removeRetailPrices() {
   const nonZeroRetailPrices = await fetchNonZeroRetailPrices().catch(log);
+  log("first check", nonZeroRetailPrices);
   const promises = nonZeroRetailPrices.map(setRetailPriceToZero);
-  const result = await Promise.allSettled(promises).catch(log);
+  const result = await Promise.allSettled(promises).catch(throwErr);
   log(result.map(({ status }) => status));
-  const secondCheck = await fetchNonZeroRetailPrices().catch(log);
+  const secondCheck = await fetchNonZeroRetailPrices().catch(throwErr);
   log("secondCheck", secondCheck);
 }
 
