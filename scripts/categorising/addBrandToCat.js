02e5c4859c9e629524d830908cc01eae7db392e2
seanomeara96 <seanom96@gmail.com>
2021-10-29 15:35:12 +0100

    more functional

--- scripts/categorising/addBrandToCat.js
+++ scripts/categorising/addBrandToCat.js
@@ -1,18 +1,26 @@
 require("../../config/config").config("bf");
 const { addCatToProduct } = require("../products/addCatToProduct");
 const { getProductsByBrand } = require("../products/getProductsByBrand");
+
+const addCatToProductsNotInCat = (products, catId) =>
+  products.map((product) => {
+    if (!product.categories.includes(catId))
+      return addCatToProduct(product.id, catId);
+  });
+const throwErr = (err) => {
+  console.log(err);
+  throw new Error(err);
+};
+async function addBrandToCat(brandName, catId) {
+  let products = await getProductsByBrand(brandName).catch(throwErr);
+  let res = await Promise.allSettled(
+    addCatToProductsNotInCat(products, catId)
+  ).catch(throwErr);
+
+  console.log(res);
+}
+
 const brandName = "Eleven Australia";
 const catId = 592;
-getProductsByBrand(brandName)
-  .then((products) => {
-    let promises = [];
-    products.forEach((product) => {
-      if (!product.categories.includes(catId)) {
-        promises.push(addCatToProduct(product.id, catId));
-      }
-    });
-    Promise.allSettled(promises)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  })
-  .catch((err) => console.log(err));
+
+addBrandToCat(brandName, catId);
