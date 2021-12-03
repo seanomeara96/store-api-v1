5d9ab54deb13c80acbeb72d662464ae8e398244f
seanomeara96 <seanom96@gmail.com>
2021-12-03 16:02:51 +0000

    upadted

--- scripts/categorising/addCategoryToSpecificProducts.js
+++ scripts/categorising/addCategoryToSpecificProducts.js
@@ -3,45 +3,27 @@ const { addCatToProduct } = require("../../functions/products/addCatToProduct");
 /**
  * issue with this script is that its prematurely returning an empty array and not the expected output from promise allsettled
  */
-const productIds = [{"Product ID":1309},
-{"Product ID":1314},
-{"Product ID":1548},
-{"Product ID":2466},
-{"Product ID":2539},
-{"Product ID":2598},
-{"Product ID":2764},
-{"Product ID":3073},
-{"Product ID":3145},
-{"Product ID":3359},
-{"Product ID":3643},
-{"Product ID":3844},
-{"Product ID":3870},
-{"Product ID":3877},
-{"Product ID":3989},
-{"Product ID":4004},
-{"Product ID":4009},
-{"Product ID":4321},
-{"Product ID":4428},
-{"Product ID":4439},
-{"Product ID":4492},
-{"Product ID":4539},
-{"Product ID":4544},
-{"Product ID":4554},
-{"Product ID":4569},
-{"Product ID":4572},
-{"Product ID":4680},
-{"Product ID":4724},
-{"Product ID":4738},
-{"Product ID":4818},
-{"Product ID":4869},
-{"Product ID":5170},
-{"Product ID":5176},
-{"Product ID":5181},
-{"Product ID":5340},
-{"Product ID":5425}]
+const productIds = [{"Product ID":4515},
+{"Product ID":5048},
+{"Product ID":5049},
+{"Product ID":5050},
+{"Product ID":5051},
+{"Product ID":5052},
+{"Product ID":5053},
+{"Product ID":5054},
+{"Product ID":5055},
+{"Product ID":5057},
+{"Product ID":5058},
+{"Product ID":5379},
+{"Product ID":5380},
+{"Product ID":5381},
+{"Product ID":5382},
+{"Product ID":5446},
+{"Product ID":5450},
+{"Product ID":5454}]
 
 
-let catId = 641; // 
+let catId = 643; // 
 /**
  * This needs to be tested before using
  * @param {object[]} productIds
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const { removePromotionFromBrandProducts } = require("./functions/content/removePromotionFromBrandProducts");
 
-require("./config/config").config("bsk");
-removePromotionFromBrandProducts("dermalogica").then((res) => console.log(`${res.filter(i => i.status === "rejected").length}/${res.length} items rejected. Will require manual check`)).catch(console.log)
+require("./config/config").config("bf");
+removePromotionFromBrandProducts("The Ordinary").then((res) => console.log(`${res.filter(i => i.status === "rejected").length}/${res.length} items rejected. Will require manual check`)).catch(console.log)
