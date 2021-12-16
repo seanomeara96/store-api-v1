624f575fd46a9d33595522d23d1c33aa8a6b8913
seanomeara96 <seanom96@gmail.com>
2021-12-16 14:25:19 +0000

    removed comments

--- scripts/front-end/christmas-countdown.js
+++ scripts/front-end/christmas-countdown.js
@@ -1,41 +1,28 @@
 function main(backgroundColor = "#ECDBEC", fontColor = "#000") {
     const getBanner = () => document.querySelector(".countdown");
-    if (getBanner()) {
-      const container = (backgroundColor, fontColor, innerText) =>
+    const container = (backgroundColor, fontColor, innerText) =>
         `<div style="background-color: ${backgroundColor}; color:${fontColor} !important; padding:3px; border-radius: 4px;">${innerText}</div>`;
+    if (getBanner()) {
       setInterval(() => {
         const today = new Date();
         const endTime = new Date("December 23, 2021 14:00:00");
         const currentTime = today.getTime();
         const endTimeAt2 = endTime.getTime();
         if (currentTime < endTimeAt2) {
-          /**
-           * divide milliseconds into seconds
-           */
           const secondsUntil2 = Math.floor((endTimeAt2 - currentTime) / 1000);
           const daysUntil = Math.floor(secondsUntil2 / 60 / 60 / 24);
-          /**
-           * full hours = seconds to minutes to hours
-           */
           const hoursUntil2 = Math.floor(
             (secondsUntil2 - daysUntil * 24 * 60 * 60) / 60 / 60
           );
-          /**
-           * minutesuntil - subtract full hours (converted into seconds) from seconds and divide by 60. The remainder should be used to calculate seconds
-           */
           const minutesUntil2 = Math.floor(
             (secondsUntil2 - (hoursUntil2 * 60 * 60 + daysUntil * 24 * 60 * 60)) /
               60
           );
-          /**
-           * seconds remaining in current minute = seconds until finish time - seconds remaining after hours and minutes subtracted (hours remaining in seconds - minutes remaining in seconds)
-           */
           const secondsRemaining = Math.floor(
             secondsUntil2 -
               (hoursUntil2 * 60 * 60 + daysUntil * 24 * 60 * 60) -
               minutesUntil2 * 60
           );
-  
           getBanner().innerHTML = container(
             backgroundColor,
             fontColor,
