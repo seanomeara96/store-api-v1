7c2ef01a352cbea02061cd0bb1a65c8c1a3e5986
seanomeara96 <seanom96@gmail.com>
2022-03-14 09:28:57 +0000

    new comments

--- functions/products/setVisibilityOfMany.js
+++ functions/products/setVisibilityOfMany.js
@@ -1,4 +1,10 @@
 const { productIsVisible } = require("./productIsVisible");
+/**
+ * set visibility of many products to either true (visible) or false (not visible)
+ * @param {object[]} productIds 
+ * @param {boolean} is_visible 
+ * @returns 
+ */
 const setVisibilityOfMany = (productIds, is_visible) =>
   new Promise((resolve, reject) =>
     Promise.allSettled(
