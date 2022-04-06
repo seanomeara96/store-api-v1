2ddc44209b926f9564560a6e02f2eb86b563cd42
seanomeara96 <seanom96@gmail.com>
2022-04-06 10:20:00 +0100

    added array type

--- functions/categories/getCategoryByName.ts
+++ functions/categories/getCategoryByName.ts
@@ -7,7 +7,7 @@ import { getAllCategories } from "./getAllCategories";
 export const getCategoryByName = (name: string) =>
   new Promise((resolve, reject) =>
     getAllCategories({ name })
-      .then((res: any) =>
+      .then((res: any[]) =>
         res.length > 1
           ? reject("there are multiple categories with this name")
           : resolve(res[0])
