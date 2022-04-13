0805359fe11f0d93a6153b2fe6f33ffe0d3366f8
seanomeara96 <seanom96@gmail.com>
2022-04-13 13:09:17 +0100

    conciseness

--- scripts/products/setVisibilityOfMany.js
+++ scripts/products/setVisibilityOfMany.js
@@ -1,23 +1,27 @@
-// require("../../config/config").config("bf");
+require("../../config/config").config("bf");
 
 const {
   setVisibilityOfMany,
 } = require("../../functions/products/setVisibilityOfMany");
 
-const products = [{"Product ID":5869},
-{"Product ID":5870},
-{"Product ID":5871}]
+const products = [
+  { "Product ID": 5665 },
+  { "Product ID": 5667 },
+  { "Product ID": 5668 },
+  { "Product ID": 5669 },
+  { "Product ID": 5670 },
+  { "Product ID": 5671 },
+  { "Product ID": 5672 },
+  { "Product ID": 5675 },
+];
 
 function main() {
   setVisibilityOfMany(products, true)
     .then((res) =>
       console.log(
-        `${res.reduce((a, c) => {
-          if (c.status === "fulfilled") {
-            return a + 1;
-          }
-          return a;
-        }, 0)}/${res.length} fulfilled`
+        `${res.reduce((a, c) => (c.status === "fulfilled" ? a + 1 : a), 0)}/${
+          res.length
+        } fulfilled`
       )
     )
     .catch(console.log);
--- scripts/sorting/applySortOrderToMany.js
+++ scripts/sorting/applySortOrderToMany.js
@@ -1,11 +1,16 @@
-// require("../../config/config").config("bf");
+require("../../config/config").config("bf");
 
 const {
   applySortOrderToMany,
 } = require("../../functions/products/applySortOrderToMany");
 
-const productIds = [{"Product ID":5869},
-{"Product ID":5870},
-{"Product ID":5871}]
+const productIds = [{"Product ID":5665},
+{"Product ID":5667},
+{"Product ID":5668},
+{"Product ID":5669},
+{"Product ID":5670},
+{"Product ID":5671},
+{"Product ID":5672},
+{"Product ID":5675}]
 
-applySortOrderToMany(productIds, 209).then(console.log).catch(console.log);
+applySortOrderToMany(productIds, 9).then(console.log).catch(console.log);
