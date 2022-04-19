8072c076cd5223f2ce30ab2e1fd3709d724f8fe8
seanomeara96 <seanom96@gmail.com>
2022-04-19 15:57:27 +0100

    added function names

--- scripts/products/setVisibilityOfMany.js
+++ scripts/products/setVisibilityOfMany.js
@@ -5,22 +5,34 @@ const {
 } = require("../../functions/products/setVisibilityOfMany");
 
 const products = [
-  { "Product ID": 5769 },
-  { "Product ID": 5770 },
-  { "Product ID": 5771 },
+  { "Product ID": 5853 },
+  { "Product ID": 5854 },
+  { "Product ID": 5855 },
+  { "Product ID": 5856 },
 ];
+
 /**
  *
  * @param {{status: "fulfilled" | "rejected";}[]} allSettledResult
  * @returns {number}
  */
 function countFulfilled(allSettledResult) {
-  return allSettledResult.reduce(
-    (a, c) => (c.status === "fulfilled" ? a + 1 : a),
-    0
-  );
+  /**
+   *
+   * @param {number} a
+   * @param {{status: "fulfilled" | "rejected";}} c
+   * @returns
+   */
+  function countIfFulfilled(a, c) {
+    return c.status === "fulfilled" ? a + 1 : a;
+  }
+  return allSettledResult.reduce(countIfFulfilled, 0);
+}
+
+function notifyFulfillmentStatus(res) {
+  console.log(`${countFulfilled(res)}/${res.length} fulfilled`);
 }
 
 setVisibilityOfMany(products, true)
-  .then((res) => console.log(`${countFulfilled(res)}/${res.length} fulfilled`))
+  .then(notifyFulfillmentStatus)
   .catch(console.log);
--- scripts/sorting/applySortOrderToMany.js
+++ scripts/sorting/applySortOrderToMany.js
@@ -4,8 +4,22 @@ const {
   applySortOrderToMany,
 } = require("../../functions/products/applySortOrderToMany");
 
-const productIds = [{"Product ID":5769},
-{"Product ID":5770},
-{"Product ID":5771}]
+const productIds = [{"Product ID":5851},
+{"Product ID":5852},
+{"Product ID":5853},
+{"Product ID":5854},
+{"Product ID":5855},
+{"Product ID":5856},
+{"Product ID":5857},
+{"Product ID":5858},
+{"Product ID":5859},
+{"Product ID":5860},
+{"Product ID":5861},
+{"Product ID":5862},
+{"Product ID":5863},
+{"Product ID":5864},
+{"Product ID":5865},
+{"Product ID":5866},
+{"Product ID":5867}]
 
-applySortOrderToMany(productIds, 1308).then(console.log).catch(console.log);
+applySortOrderToMany(productIds, 1621).then(console.log).catch(console.log);
