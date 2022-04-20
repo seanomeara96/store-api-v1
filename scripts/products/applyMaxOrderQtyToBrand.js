c53174192a4ca075c4db0f21ac3c6cca8a8e2991
seanomeara96 <seanom96@gmail.com>
2022-04-20 09:03:10 +0100

    improved max cap script

--- scripts/products/applyMaxOrderQtyToBrand.js
+++ scripts/products/applyMaxOrderQtyToBrand.js
@@ -1,9 +1,7 @@
-// add max 5 limit to products
-require("./config/config").config("bf");
 const {
   getProductsByBrand,
-} = require("./functions/products/getProductsByBrand");
-const { updateProduct } = require("./functions/products/updateProduct");
+} = require("../../functions/products/getProductsByBrand");
+const { updateProduct } = require("../../functions/products/updateProduct");
 
 const applyMaxOrderQty = (product_id, qty) =>
   new Promise((resolve, reject) =>
@@ -14,11 +12,66 @@ const applyMaxOrderQty = (product_id, qty) =>
 
 const applyMaxFiveLimit = (products) =>
   new Promise((resolve, reject) => {
-    Promise.allSettled(products.map(({ id }) => applyMaxOrderQty(id, 5)))
+    const qualifiers = products.filter(function isOrderQtyLessThan5 (product) {
+      return product.order_quantity_maximum < 5;
+    });
+    Promise.allSettled(qualifiers.map(({ id }) => applyMaxOrderQty(id, 5)))
       .then(resolve)
       .catch(reject);
   });
 
-getProductsByBrand("Kerastase").then((products) => {
-    applyMaxFiveLimit(products).then(console.log).catch(console.log)
-}).catch(console.log)
+function notifyFulfillmentStatus(res) {
+  /**
+   *
+   * @param {{status: "fulfilled" | "rejected";}[]} allSettledResult
+   * @returns {number}
+   */
+  function countFulfilled(allSettledResult) {
+    /**
+     *
+     * @param {number} a
+     * @param {{status: "fulfilled" | "rejected";}} c
+     * @returns
+     */
+    function countIfFulfilled(a, c) {
+      return c.status === "fulfilled" ? a + 1 : a;
+    }
+    return allSettledResult.reduce(countIfFulfilled, 0);
+  }
+
+  console.log(`${countFulfilled(res)}/${res.length} fulfilled`);
+}
+
+// add max 5 limit to products
+function applyMaxOderQtyToBrand(store, brandName) {
+  return new Promise(function (resolve, reject) {
+    require("../../config/config").config(store);
+    getProductsByBrand(brandName)
+      .then((products) => {
+        applyMaxFiveLimit(products)
+          .then(notifyFulfillmentStatus)
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+}
+
+const storeDetails = [
+  {
+    store: "bf",
+    brand: "Kerastase",
+  },
+  {
+    store: "ah",
+    brand: "Kerastase",
+  },
+];
+
+const promises = storeDetails.map(function (dt) {
+  return () => applyMaxOderQtyToBrand(dt.store, dt.brand);
+});
+
+promises.reduce(function promiseReducer(a, c) {
+  return a.then(c);
+}, Promise.resolve());
