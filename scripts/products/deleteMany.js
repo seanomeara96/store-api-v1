2f88d53e71a2421b311218f2cf2506b1fd7d0a84
seanomeara96 <seanom96@gmail.com>
2022-05-13 09:31:58 +0100

    deleted dolce & gabanna

--- scripts/products/deleteMany.js
+++ scripts/products/deleteMany.js
@@ -2,12 +2,25 @@ const {
   deleteManyProducts,
 } = require("../../functions/products/deleteManyProducts");
 
-require("../../config/config").config("pb");
+require("../../config/config").config("bf");
 
 function deleteMany() {
-  const products = [{"Product ID":493},
-  {"Product ID":494},
-  {"Product ID":1240}]
+  const products = [
+    { bc_id: 5734 },
+    { bc_id: 5735 },
+    { bc_id: 5736 },
+    { bc_id: 5737 },
+    { bc_id: 5738 },
+    { bc_id: 5739 },
+    { bc_id: 5740 },
+    { bc_id: 5741 },
+    { bc_id: 5742 },
+    { bc_id: 5743 },
+    { bc_id: 5744 },
+    { bc_id: 5745 },
+    { bc_id: 5746 },
+    { bc_id: 5747 },
+  ];
 
   deleteManyProducts(products).then(console.log).catch(console.log);
 }
