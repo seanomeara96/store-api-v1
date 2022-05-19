7163be0f3b608e5376acd3381cff500d8fd1d9be
seanomeara96 <seanom96@gmail.com>
2022-05-19 15:54:11 +0100

    formatting

--- scripts/promotions/utils.js
+++ scripts/promotions/utils.js
@@ -1,17 +1,17 @@
 function percentageDiscount(priceToReduce, percentageAsDecimal) {
-    return Math.round(priceToReduce * (1 - percentageAsDecimal) * 100) / 100;
-  }
+  return Math.round(priceToReduce * (1 - percentageAsDecimal) * 100) / 100;
+}
 
 exports.percentageDiscount = percentageDiscount;
-  
-  function getPrices(el, discountFn, discount) {
-    return {
-      default_price: el.price,
-      id: el.id,
-      retail_price: el.retail_price,
-      sale_price: el.sale_price,
-      promo_price: discountFn(el.price, discount),
-    };
-  }
 
-  exports.getPrices = getPrices;
+function getPrices(el, discountFn, discount) {
+  return {
+    default_price: el.price,
+    id: el.id,
+    retail_price: el.retail_price,
+    sale_price: el.sale_price,
+    promo_price: discountFn(el.price, discount),
+  };
+}
+
+exports.getPrices = getPrices;
