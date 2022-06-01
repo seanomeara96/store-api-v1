346e39328af7317fca921af41e507239f6d9edd2
seanomeara96 <seanom96@gmail.com>
2022-06-01 10:36:41 +0100

    gwp updates

--- scripts/products/delete.js
+++ scripts/products/delete.js
@@ -1,4 +1,4 @@
-require("../../config/config").config("ih");
+require("../../config/config").config("ah");
 const { deleteProduct } = require("../../functions/products/deleteProduct");
-deleteProduct(3255).then((res) => console.log(res + "\n")).catch(console.log);
+deleteProduct(1756).then((res) => console.log(res + "\n")).catch(console.log);
 
