d9b890d3592ac47a37cd109a09f89d2804af89ef
seanomeara96 <seanom96@gmail.com>
2022-07-22 16:06:16 +0100

    price updates

--- test.js
+++
@@ -1,5 +0,0 @@
-require("./config/config").config("bf");
-
-const { getAllProducts } = require("./functions/products/getAllProducts");
-
-getAllProducts({"categories:in": 702}).then(res => console.log(res.map(p => p.id)))
