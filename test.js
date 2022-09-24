04087d8c4e4bec04fb214c6e128ad30b00aaff62
seanomeara96 <seanom96@gmail.com>
2022-09-24 12:46:02 +0100

    tested email

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-require("./config/config").config("ss");
-const { getAllProducts } = require("./functions/products/getAllProducts");
-const { updateProduct } = require("./functions/products/updateProduct");
-getAllProducts().then(main);
+require("./config/config");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function main(products) {
-  for(const p of products){
-    await updateProduct(p.id, {description: p.description.replace('<h2>Description</h2>', "")}).catch(console.log)
-  }
-  console.log("done")
-}
+(async function () {
+  const msg = {
+    to: "david@dcphysiotherapy.ie",
+    from: "info@dcphysiotherapy.ie",
+    cc: "sean@beautyfeatures.ie",
+    subject: "Site Updates",
+    text: "Hi David, I'm doing some site updates over the next few days. I'm currently testing out this new email client to make sure this reaches you without being filtered out by anti-spam tools.",
+    // html: data,
+  };
+  sgMail.send(msg).then(console.log).catch(console.log);
+})();
