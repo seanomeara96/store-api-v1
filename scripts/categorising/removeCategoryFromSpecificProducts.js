45487ef1454e803ded3cb35ec258b0728866ba0a
seanomeara96 <seanom96@gmail.com>
2022-11-17 12:15:38 +0000

    refactored script

--- scripts/categorising/removeCategoryFromSpecificProducts.js
+++ scripts/categorising/removeCategoryFromSpecificProducts.js
@@ -14,30 +14,24 @@ const {
  */
 const removeCategoryFromSpecificProducts = (productIds, categoryId) =>
   new Promise((resolve, reject) => {
-    let promises = [];
-    productIds.forEach((productId) => {
-      let id = productId[Object.keys(productId)[0]];
-      promises.push(removeCatFromProduct(id, categoryId));
+    const promises = productIds.map((productId) => {
+      const id = Object.values(productId)[0];
+      return removeCatFromProduct(id, categoryId);
     });
     Promise.allSettled(promises).then(resolve).catch(reject);
   });
 
+const catId = 514;
 
-const productIds = [{"Product ID":3404},
-{"Product ID":5332},
-{"Product ID":5253},
-{"Product ID":1772},
-{"Product ID":3156},
-{"Product ID":3455},
-{"Product ID":3451},
-{"Product ID":222},
-{"Product ID":5920},
-{"Product ID":6334},
-{"Product ID":5795},
-{"Product ID":4312}];
+const productIds = [
+  { id: 3506 },
+  { id: 3509 },
+  { id: 3511 },
+  { id: 3580 },
+  { id: 3577 },
+  { id: 3582 },
+];
 
-
-const catId = 640;
 removeCategoryFromSpecificProducts(productIds, catId)
   .then((res) =>
     console.log("removeCategoryFromSpecificProducts response", res)
