87471c09a5ad5ff8ef75de7edb142c3a2de5c8ea
seanomeara96 <seanom96@gmail.com>
2023-01-17 10:58:20 +0000

    updated countdown script

--- scripts/front-end/countdown.js
+++ scripts/front-end/countdown.js
@@ -1,16 +1,5 @@
 window.addEventListener("DOMContentLoaded", async () => {
   const publicHolidays = [
-    "21 December 2022",
-    "22 December 2022",
-    "23 December 2022",
-    "24 December 2022",
-    "25 December 2022",
-    "26 December 2022",
-    "27 December 2022",
-    "28 December 2022",
-    "29 December 2022",
-    "30 December 2022",
-    "31 December 2022",
     "1 January 2022",
     "2 January 2022",
     "3 January 2022",
@@ -27,7 +16,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 
   // this should work
   function getContainer() {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       let count = 0;
       const qry = () => document.querySelector(".countdown");
       let cntnr = qry();
@@ -40,53 +29,57 @@ window.addEventListener("DOMContentLoaded", async () => {
         }
         if (count === 4) {
           clearInterval(itr);
-          resolve(undefined);
+          reject("no countdown container");
         }
         count++;
       }, 2000);
     });
   }
 
-  const container = await getContainer();
+  try {
+    const container = await getContainer();
 
-  const today = new Date();
+    const today = new Date();
 
-  const todayIsNotWeekend = ![0, 5, 6].includes(today.getDay());
+    const todayIsNotWeekend = ![0, 5, 6].includes(today.getDay());
 
-  const todayIsNotPublicHoliday = !publicHolidays.includes(
-    new Intl.DateTimeFormat("en-IE", {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    }).format(today)
-  );
+    const todayIsNotPublicHoliday = !publicHolidays.includes(
+      new Intl.DateTimeFormat("en-IE", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }).format(today)
+    );
 
-  const todayAtEnd = new Date(
-    `${today.toString().replace(/[0-9]+:[0-9]+:[0-9]+/, "14:00:00")}`
-  );
+    const todayAtEnd = new Date(
+      `${today.toString().replace(/[0-9]+:[0-9]+:[0-9]+/, "14:00:00")}`
+    );
 
-  const timeAtEnd = todayAtEnd.getTime();
+    const timeAtEnd = todayAtEnd.getTime();
 
-  if (todayIsNotWeekend && todayIsNotPublicHoliday && container) {
-    setInterval(() => {
-      const currentTime = new Date().getTime();
-      if (timeAtEnd > currentTime) {
-        const secondsUntilEnd = Math.floor((timeAtEnd - currentTime) / 1000);
+    if (todayIsNotWeekend && todayIsNotPublicHoliday && container) {
+      setInterval(() => {
+        const currentTime = new Date().getTime();
+        if (timeAtEnd > currentTime) {
+          const secondsUntilEnd = Math.floor((timeAtEnd - currentTime) / 1000);
 
-        const hoursUntilEnd = Math.floor(secondsUntilEnd / 60 / 60);
+          const hoursUntilEnd = Math.floor(secondsUntilEnd / 60 / 60);
 
-        const minutesUntilEnd = Math.floor(
-          (secondsUntilEnd - hoursUntilEnd * 60 * 60) / 60
-        );
+          const minutesUntilEnd = Math.floor(
+            (secondsUntilEnd - hoursUntilEnd * 60 * 60) / 60
+          );
 
-        const secondsRemaining = Math.floor(
-          secondsUntilEnd - hoursUntilEnd * 60 * 60 - minutesUntilEnd * 60
-        );
+          const secondsRemaining = Math.floor(
+            secondsUntilEnd - hoursUntilEnd * 60 * 60 - minutesUntilEnd * 60
+          );
 
-        container.innerHTML = `&#128666; ${hoursUntilEnd} hrs ${minutesUntilEnd} mins ${secondsRemaining} secs left for Next Day Delivery`;
-      }
-    }, 1000);
-  } else {
-    container.innerHTML = "&#128666; Order before 2pm for Next Day Delivery";
+          container.innerHTML = `&#128666; ${hoursUntilEnd} hrs ${minutesUntilEnd} mins ${secondsRemaining} secs left for Next Day Delivery`;
+        }
+      }, 1000);
+    } else {
+      container.innerHTML = "&#128666; Order before 2pm for Next Day Delivery";
+    }
+  } catch (err) {
+    console.error(err);
   }
 });
