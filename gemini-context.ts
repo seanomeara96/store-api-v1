4fcc3b533fc46f4f8c3f47c3c82c7449f798c8b2
seanomeara96 <seanom96@gmail.com>
2025-02-14 11:00:20 +0000

    .

--- gemini-context.ts
+++ gemini-context.ts
@@ -1,24 +1,23 @@
 import { htmlToText } from "html-to-text";
-import { Category } from "./functions/categories/createCategory";
-import { getAllCategories } from "./functions/categories/getAllCategories";
+
 import { getAllProducts } from "./functions/products/getAllProducts";
 import { output } from "./scripts/utils/output";
-import fs from "fs"
+
 import path from "path"
 import { getProductVariants } from "./functions/products/getProductVariants";
 
 
-const store = "ah"
+const store = "bf"
 
 async function main() {
   try {
     require("./config/config").config(store);
    
     let products = await getAllProducts()
+
     products = products.filter(p => p.inventory_level)
 
-
-    let out = []
+    let out = [];
 
     for(const product of products){
       const variants = await getProductVariants(product.id)
@@ -27,13 +26,15 @@ async function main() {
           sku: variant.sku,
           name: product.name,
           description: htmlToText(product.description),
+          retail_price: product.price,
           sale_price: product.sale_price,
+          saving: String(Math.round(((product.price - product.sale_price) / product.price) * 100)) + "%"
         })
       }
     }
 
     
-    output(path.resolve(__dirname, "ah-bare-catalog.csv"), out,true)
+    output(path.resolve(__dirname, `${store}-bare-catalog.csv`), out,true)
   } catch (err) {
     console.log(err);
   }
